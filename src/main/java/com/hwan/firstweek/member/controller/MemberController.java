b992b1d5b467334cf5985add1f7d93e0fbfd38fb
hwanguebest@naver.com <hwanguebest@naver.com>
2023-01-09 17:18:08 +0900

    init: 프로젝트 생성_환규

---
+++ src/main/java/com/hwan/firstweek/member/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.hwan.firstweek.member.controller;
+
+import com.hwan.firstweek.jwt.TokenInfo;
+import com.hwan.firstweek.member.dto.MemberLoginRequestDto;
+import com.hwan.firstweek.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public TokenInfo login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+        String memberId = memberLoginRequestDto.getMemberId();
+        String password = memberLoginRequestDto.getPassword();
+        TokenInfo tokenInfo = memberService.login(memberId, password);
+        return tokenInfo;
+    }
+}
---
+++ src/main/java/com/hwan/firstweek/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.hwan.firstweek.member.repository;
+
+import com.hwan.firstweek.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMemberId(String username);
+}
